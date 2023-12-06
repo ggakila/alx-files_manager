81859a112070f1288a13ef303ec0308f5fbdc20e
ggakila <gisorehaggai@gmail.com>
2023-12-06 21:15:46 +0300

    trial 5 redis utils

--- utils/redis.js
+++ utils/redis.js
@@ -1,4 +1,5 @@
 import redis from "redis";
+import { promisify } from "util";
 
 const HOST = process.env.REDIS_HOST || "localhost";
 const PORT = process.env.REDIS_PORT || 5000;
@@ -17,17 +18,17 @@ class RedisClient {
 	}
 
 	async get(key) {
-		const asyncGet = await(this.client.get).bind(this.client);
+		const asyncGet = promisify(this.client.get).bind(this.client);
 		return asyncGet(key);
 	}
 
 	async set(key, value, durationInSeconds) {
-		const asyncSet = await(this.client.set).bind(this.client);
+		const asyncSet = promisify(this.client.set).bind(this.client);
 		return asyncSet(key, value, "EX", durationInSeconds);
 	}
 
 	async del(key) {
-		const asyncDel = await(this.client.del).bind(this.client);
+		const asyncDel = promisify(this.client.del).bind(this.client);
 		return asyncDel(key);
 	}
 }
