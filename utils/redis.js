a0888aabf7b9eaa86624906490dfcf9540a8d4dc
ggakila <gisorehaggai@gmail.com>
2023-12-06 21:25:05 +0300

    redis utils shege time trial 8

--- utils/redis.js
+++ utils/redis.js
@@ -21,7 +21,8 @@ class RedisClient {
 
 	async set(key, value, durationInSeconds) {
 		const asyncSet = promisify(this.client.set).bind(this.client);
-		return asyncSet(key, value, "EX", durationInSeconds);
+		await asyncSet(key, value, "EX", durationInSeconds);
+        await this.client.expire(key, durationInSeconds);
 	}
 
 	async del(key) {
