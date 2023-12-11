d9df68279f47fef9d0d605cc163096ef50f9a116
ggakila <gisorehaggai@gmail.com>
2023-12-11 23:18:28 +0300

    eslint indents

--- utils/redis.js
+++ utils/redis.js
@@ -1,11 +1,11 @@
-import { createClient } from "redis";
-import { promisify } from "util";
+import { createClient } from 'redis';
+import { promisify } from 'util';
 
 class RedisClient {
 	constructor() {
 		this.client = createClient();
-		this.client.on("error", (error) => {
-			console.log(`Redis client not connected to server: ${error}`);
+		this.client.on('error', (error) => {
+      console.log(`Redis client not connected to server: ${error}`);
 		});
 	}
 
