32f5b25d59bc64317d208ab5e406bd1b858aa76a
ggakila <gisorehaggai@gmail.com>
2023-12-11 23:48:11 +0300

    es lint changes

--- utils/db.js
+++ utils/db.js
@@ -1,52 +1,50 @@
-import { MongoClient } from "mongodb";
+import { MongoClient } from 'mongodb';
 
-const DB_HOST = process.env.DB_HOST || "localhost";
+const DB_HOST = process.env.DB_HOST || 'localhost';
 const DB_PORT = process.env.DB_PORT || 27017;
-const DB_DATABASE = process.env.DB_DATABASE || "files_manager";
+const DB_DATABASE = process.env.DB_DATABASE || 'files_manager';
 
 class DBClient {
-	constructor() {
-		
-		this.client = new MongoClient(`mongodb://${DB_HOST}:${DB_PORT}`, {
-			useUnifiedTopology: true,
-			useNewUrlParser: true,
-		});
+  constructor() {
+    this.client = new MongoClient(`mongodb://${DB_HOST}:${DB_PORT}`, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    });
 
-		
-		this.client.connect((err) => {
-			if (!err) {
-				this.db = this.client.db(`${DB_DATABASE}`);
-				this.users = this.db.collection("users");
-				this.files = this.db.collection("files");
-			} else {
-				console.error(`MongoDB connection error: ${err.message}`);
-				this.db = null;
-			}
-		});
-	}
+    this.client.connect((err) => {
+      if (!err) {
+        this.db = this.client.db(`${DB_DATABASE}`);
+        this.users = this.db.collection('users');
+        this.files = this.db.collection('files');
+      } else {
+        console.error(`MongoDB connection error: ${err.message}`);
+        this.db = null;
+      }
+    });
+  }
 
-	isAlive() {
-		return !!this.db; 
-	}
+  isAlive() {
+    return !!this.db;
+  }
 
-	async nbUsers() {
-		if (!this.db) {
-			return 0;
-		}
+  async nbUsers() {
+    if (!this.db) {
+      return 0;
+    }
 
-		const usersNum = await this.users.countDocuments();
-		return usersNum;
-	}
+    const usersNum = await this.users.countDocuments();
+    return usersNum;
+  }
 
-	async nbFiles() {
-		if (!this.db) {
-			return 0;
-		}
+  async nbFiles() {
+    if (!this.db) {
+      return 0;
+    }
 
-		const filesNum = await this.files.countDocuments();
-		return filesNum;
-	}
+    const filesNum = await this.files.countDocuments();
+    return filesNum;
+  }
 }
 
 const dbClient = new DBClient();
-module.exports =  dbClient;
+module.exports = dbClient;
