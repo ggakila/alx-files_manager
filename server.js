d31baeca4fa0b4a039211a95e053215678102e09
ggakila <gisorehaggai@gmail.com>
2023-12-12 16:57:11 +0300

    esssss liint

--- server.js
+++ server.js
@@ -1,14 +1,10 @@
-import express from 'express';
-import routes from './routes/index';
+const express = require('express');
+const routes = require('./routes/index');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
-app.use(express.json());
 app.use('/', routes);
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
-
-export default app;
