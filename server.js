a1f40b89a35f8ed8eaca0c3805bb049ddc8bd0e6
ggakila <gisorehaggai@gmail.com>
2023-12-12 17:03:47 +0300

    back up

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
-const express = require('express');
-const routes = require('./routes/index');
+import express from 'express';
+import routes from './routes/index';
 
 const app = express();
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
+app.use(express.json());
 app.use('/', routes);
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
+
+export default app;
