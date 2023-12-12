2955ac784588307248ac587ab1ec29fce781458e
ggakila <gisorehaggai@gmail.com>
2023-12-12 17:14:39 +0300

    files manager

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import routes from './routes/index';
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = parseInt(process.env.PORT, 10) || 5000;
 
 app.use(express.json());
 app.use('/', routes);
